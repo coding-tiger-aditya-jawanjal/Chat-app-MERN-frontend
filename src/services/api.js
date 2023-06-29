92cf0ac086ed6469f2fc23b1ed23007052695935
Aditya Jawanjal <adityajawanjal01@gmail.com>
2023-06-29 12:08:03 +0530

    mal

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const key = localStorage.getItem("chatUser");
 const token = JSON.parse(key);
 
-export const BACKEND_URL = `http://localhost:5000`;
+export const BACKEND_URL = `https://chat-app-mern-backend.onrender.com`;
 // export const BACKEND_URL = `https://chat-app-mern-backend.onrender.com`;
 const Api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
